3ab46919363ce89d5d5ad97baeab4690165238a5
Catriona Reader <creader@ckmanalytix.com>
2020-02-27 14:11:05 +0000

    adding and removing elements on bar

--- bars_add_and_exit_with_kv_pairs/theScript.js
+++ bars_add_and_exit_with_kv_pairs/theScript.js
@@ -1,8 +1,30 @@
 var w = 600;
 var h = 250;
 
-var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
-                11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+var dataset = [ { key: 0, value: 5 },		//dataset is now an array of objects.
+                { key: 1, value: 10 },		//Each object has a 'key' and a 'value'.
+                { key: 2, value: 13 },
+                { key: 3, value: 19 },
+                { key: 4, value: 21 },
+                { key: 5, value: 25 },
+                { key: 6, value: 22 },
+                { key: 7, value: 18 },
+                { key: 8, value: 15 },
+                { key: 9, value: 13 },
+                { key: 10, value: 11 },
+                { key: 11, value: 12 },
+                { key: 12, value: 15 },
+                { key: 13, value: 20 },
+                { key: 14, value: 18 },
+                { key: 15, value: 17 },
+                { key: 16, value: 16 },
+                { key: 17, value: 18 },
+                { key: 18, value: 23 },
+                { key: 19, value: 25 } ];
+
+const key = function(d) {
+    return d.key; 
+}
 
 var xScale = d3.scaleBand()
                 .domain(d3.range(dataset.length))
@@ -10,7 +32,7 @@ var xScale = d3.scaleBand()
                 .paddingInner(0.05);
 
 var yScale = d3.scaleLinear()
-                .domain([0, d3.max(dataset)])
+                .domain([0, d3.max(dataset, d => d.value)])
                 .range([0, h]);
 
 //Create SVG element
@@ -21,184 +43,281 @@ var svg = d3.select("body")
 
 //Create bars
 svg.selectAll("rect")
-    .data(dataset)
+    .data(dataset, key)
     .enter()
     .append("rect")
     .attr("x", function(d, i) {
         return xScale(i);
     })
     .attr("y", function(d) {
-        return h - yScale(d);
+        return h - yScale(d.value);
     })
     .attr("width", xScale.bandwidth())
     .attr("height", function(d) {
-        return yScale(d);
+        return yScale(d.value);
     })
     .attr("fill", function(d) {
-        return "rgb(0, 0, " + Math.round(d * 10) + ")";
+        return "rgb(0, 0, " + Math.round(d.value * 10) + ")";
     });
 
 //Create labels
 svg.selectAll("text")
-    .data(dataset)
+    .data(dataset, key)
     .enter()
     .append("text")
-    .text(function(d) {
-        return d;
-    })
+    .text(d => d.value)
     .attr("text-anchor", "middle")
     .attr("x", function(d, i) {
         return xScale(i) + xScale.bandwidth() / 2;
     })
     .attr("y", function(d) {
-        if (yScale(d) < 30) {
-            return h - yScale(d) - 14;
+        if (yScale(d.value) < 30) {
+            return h - yScale(d.value) - 14;
         } else {
-            return h - yScale(d) + 14;
+            return h - yScale(d.value) + 14;
         }
     })
     .attr("font-family", "sans-serif")
     .attr("font-size", "11px")
     .attr("fill", function(d) {
-        if (yScale(d) < 30) {
+        if (yScale(d.value) < 30) {
             return "black";
         } else {
             return "white";
         }
     });
 
-d3.select("#add_one")
+d3.selectAll("button")
     .on("click", function() {
-
-        const maxValue = 20; 
-        const newNumber = Math.floor(Math.random() * maxValue); 
-        dataset.push(newNumber); 
-
-        xScale.domain(d3.range(dataset.length))
-
-        const bars = svg.selectAll("rect")
-                        .data(dataset); //binding all the new data
     
-        bars.enter() // now only referring to the new element 
-            .append("rect")
-            .attr("x", w) // keeping it out of sight
-            .attr("y", d => h - yScale(d)) // setting it to the correc y co-ord
-            .attr("width", xScale.bandwidth())
-            .attr("height", d => yScale(d))
-            .attr("fill", function(d)  {
-                return `rgb(0, 0, ${Math.round(d*10)})`
-            })
-            .merge(bars) // now bringing this single element into the rest of the bars
-            .transition().duration(500)
-            .attr("x", function(d, i) {
-                return xScale(i); 
-            })
-            .attr("y", d => h - yScale(d))
-            .attr("width", xScale.bandwidth())
-            .attr("height", d => yScale(d))
+    const buttonId = d3.select(this).attr("id"); 
 
-        const text = svg.selectAll("text")
-                        .data(dataset); 
-        
-        text.enter()
-            .append("text")
-            .text(d => d)
-            .attr("text-anchor", "middle")
-            .attr("x", w)
-            .attr("y", function(d) {
-                if (yScale(d) < 30) {
-                    return h - yScale(d) - 14;
-                } else {
-                    return h - yScale(d) + 14;
-                }
-            })
-            .attr("font-family", "sans-serif")
-            .attr("font-size", "11px")
-            .attr("fill", function(d) {
-                if (yScale(d) < 30) {
-                    return "black";
-                } else {
-                    return "white";
-                }
-            })
-            .merge(text) // and remember, this is the variable defined above
-            .transition().duration(500)
-            .attr("x", function(d, i) {
-                return xScale(i) + xScale.bandwidth() / 2;
-            });
+    if (buttonId == "add_one") {
+
+        const minValue  = 2; 
+        const maxValue = 25 - minValue; 
+        const newNumber = Math.floor(Math.random() * maxValue) + minValue; 
+        const lastKeyValue = dataset[dataset.length - 1].key; 
+        dataset.push({key: lastKeyValue + 1, 
+                        value: newNumber}); 
+
+    } else {
+        dataset.shift()
+    }
+
+    xScale.domain(d3.range(dataset.length))
+
+    const bars = svg.selectAll("rect")
+                        .data(dataset, key); //binding all the new data
+
+    bars.enter() // now only referring to the new element 
+        .append("rect")
+        .attr("x", w) // keeping it out of sight
+        .attr("y", function(d) {
+            return h - yScale(d.value); 
+        }) 
+        .attr("width", xScale.bandwidth())
+        .attr("height", d => yScale(d.value))
+        .attr("fill", function(d)  {
+            return `rgb(0, 0, ${Math.round(d.value *10)})`
+        })
+        .merge(bars) // now bringing this single element into the rest of the bars
+        .transition().duration(500)
+        .attr("x", function(d, i) {
+            return xScale(i); 
+        })
+        .attr("y", function(d) {
+            return h - yScale(d.value); 
+        }) 
+        .attr("width", xScale.bandwidth())
+        .attr("height", d => yScale(d.value))
+
+    bars.exit() // now only referring to the new element 
+        .transition()
+        .duration(500)
+        .attr("x", 0) // keeping it out of sight
+        .remove();
+    
+    const text = svg.selectAll("text")
+                    .data(dataset, key); 
+    
+    text.enter()
+        .append("text")
+        .text(d => d.value)
+        .attr("text-anchor", "middle")
+        .attr("x", w)
+        .attr("y", function(d) {
+            if (yScale(d.value) < 30) {
+                return h - yScale(d.value) - 14;
+            } else {
+                return h - yScale(d.value) + 14;
+            }
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("fill", function(d) {
+            if (yScale(d.value) < 30) {
+                return "black";
+            } else {
+                return "white";
+            }
+        })
+        .merge(text) // and remember, this is the variable defined above
+        .transition().duration(500)
+        .attr("x", function(d, i) {
+            return xScale(i) + xScale.bandwidth() / 2;
+        });
+
+
+    text.exit()
+        .transition()
+        .duration(500)
+        .attr("x", 0)
+        .remove(); 
+
+
     }); 
 
+// d3.select("#add_one")
+//     .on("click", function() {
 
-// weird, but remember that even though we want to remove an element, we still 
-// need to enter the updated data before exiting the extra data 
-d3.select("#remove_one")
-    .on("click", function() {
+//         const maxValue = 20; 
+//         const newNumber = Math.floor(Math.random() * maxValue); 
+//         dataset.push(newNumber); 
 
-        dataset.pop()
+//         xScale.domain(d3.range(dataset.length))
 
-        xScale.domain(d3.range(dataset.length))
-
-        const bars = svg.selectAll("rect")
-                        .data(dataset); //binding all the new data
-
-        bars.enter() // now only referring to the new element 
-            .append("rect")
-            .attr("x", w) // keeping it out of sight
-            .attr("y", d => h - yScale(d)) // setting it to the correc y co-ord
-            .attr("width", xScale.bandwidth())
-            .attr("height", d => yScale(d))
-            .attr("fill", function(d)  {
-                return `rgb(0, 0, ${Math.round(d*10)})`
-            })
-            .merge(bars) // now bringing this single element into the rest of the bars
-            .transition().duration(500)
-            .attr("x", function(d, i) {
-                return xScale(i); 
-            })
-            .attr("y", d => h - yScale(d))
-            .attr("width", xScale.bandwidth())
-            .attr("height", d => yScale(d))
+//         const bars = svg.selectAll("rect")
+//                         .data(dataset, key); //binding all the new data
     
-        bars.exit() // now only referring to the new element 
-            .transition()
-            .duration(500)
-            .attr("x", w) // keeping it out of sight
-            .remove();
+//         bars.enter() // now only referring to the new element 
+//             .append("rect")
+//             .attr("x", w) // keeping it out of sight
+//             .attr("y", function(d) {
+//                 return h - yScale(d.value); 
+//             }) 
+//             .attr("width", xScale.bandwidth())
+//             .attr("height", d => yScale(d.value))
+//             .attr("fill", function(d)  {
+//                 return `rgb(0, 0, ${Math.round(d.value*10)})`
+//             })
+//             .merge(bars) // now bringing this single element into the rest of the bars
+//             .transition().duration(500)
+//             .attr("x", function(d, i) {
+//                 return xScale(i); 
+//             })
+//             .attr("y", function(d) {
+//                 return h - yScale(d.value); 
+//             }) 
+//             .attr("width", xScale.bandwidth())
+//             .attr("height", d => yScale(d.value))
+
+//         const text = svg.selectAll("text")
+//                         .data(dataset, key); 
+        
+//         text.enter()
+//             .append("text")
+//             .text(d => d.value)
+//             .attr("text-anchor", "middle")
+//             .attr("x", w)
+//             .attr("y", function(d) {
+//                 if (yScale(d.value) < 30) {
+//                     return h - yScale(d.value) - 14;
+//                 } else {
+//                     return h - yScale(d.value) + 14;
+//                 }
+//             })
+//             .attr("font-family", "sans-serif")
+//             .attr("font-size", "11px")
+//             .attr("fill", function(d) {
+//                 if (yScale(d.value) < 30) {
+//                     return "black";
+//                 } else {
+//                     return "white";
+//                 }
+//             })
+//             .merge(text) // and remember, this is the variable defined above
+//             .transition().duration(500)
+//             .attr("x", function(d, i) {
+//                 return xScale(i) + xScale.bandwidth() / 2;
+//             });
+//     }); 
+
+
+// // weird, but remember that even though we want to remove an element, we still 
+// // need to enter the updated data before exiting the extra data 
+// d3.select("#remove_one")
+//     .on("click", function() {
+
+//         dataset.pop()
+
+//         xScale.domain(d3.range(dataset.length))
+
+//         const bars = svg.selectAll("rect")
+//                         .data(dataset, key); //binding all the new data
+
+//         bars.enter() // now only referring to the new element 
+//             .append("rect")
+//             .attr("x", w) // keeping it out of sight
+//             .attr("y", function(d) {
+//                 return h - yScale(d.value); 
+//             }) 
+//             .attr("width", xScale.bandwidth())
+//             .attr("height", d => yScale(d.value))
+//             .attr("fill", function(d)  {
+//                 return `rgb(0, 0, ${Math.round(d.value *10)})`
+//             })
+//             .merge(bars) // now bringing this single element into the rest of the bars
+//             .transition().duration(500)
+//             .attr("x", function(d, i) {
+//                 return xScale(i); 
+//             })
+//             .attr("y", function(d) {
+//                 return h - yScale(d.value); 
+//             }) 
+//             .attr("width", xScale.bandwidth())
+//             .attr("height", d => yScale(d.value))
+    
+//         bars.exit() // now only referring to the new element 
+//             .transition()
+//             .duration(500)
+//             .attr("x", w) // keeping it out of sight
+//             .remove();
       
-        const text = svg.selectAll("text")
-                        .data(dataset); 
+//         const text = svg.selectAll("text")
+//                         .data(dataset, key); 
         
-        text.enter()
-            .append("text")
-            .text(d => d)
-            .attr("text-anchor", "middle")
-            .attr("x", w)
-            .attr("y", function(d) {
-                if (yScale(d) < 30) {
-                    return h - yScale(d) - 14;
-                } else {
-                    return h - yScale(d) + 14;
-                }
-            })
-            .attr("font-family", "sans-serif")
-            .attr("font-size", "11px")
-            .attr("fill", function(d) {
-                if (yScale(d) < 30) {
-                    return "black";
-                } else {
-                    return "white";
-                }
-            })
-            .merge(text) // and remember, this is the variable defined above
-            .transition().duration(500)
-            .attr("x", function(d, i) {
-                return xScale(i) + xScale.bandwidth() / 2;
-            });
+//         text.enter()
+//             .append("text")
+//             .text(d => d.value)
+//             .attr("text-anchor", "middle")
+//             .attr("x", w)
+//             .attr("y", function(d) {
+//                 if (yScale(d.value) < 30) {
+//                     return h - yScale(d.value) - 14;
+//                 } else {
+//                     return h - yScale(d.value) + 14;
+//                 }
+//             })
+//             .attr("font-family", "sans-serif")
+//             .attr("font-size", "11px")
+//             .attr("fill", function(d) {
+//                 if (yScale(d.value) < 30) {
+//                     return "black";
+//                 } else {
+//                     return "white";
+//                 }
+//             })
+//             .merge(text) // and remember, this is the variable defined above
+//             .transition().duration(500)
+//             .attr("x", function(d, i) {
+//                 return xScale(i) + xScale.bandwidth() / 2;
+//             });
 
 
-        text.exit()
-            .transition()
-            .duration(500)
-            .attr("x", w)
-            .remove(); 
-    }); 
+//         text.exit()
+//             .transition()
+//             .duration(500)
+//             .attr("x", w)
+//             .remove(); 
+//     }); 
